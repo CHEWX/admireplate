e550d26052dfd63aa150022395269de994d3d133
chewx <tomhop88@btinternet.com>
2014-08-06 13:27:14 +0100

    add mamp specific if statement for javascript

--- index.php
+++ index.php
@@ -7,18 +7,33 @@
 	<meta charset="utf-8">
 	<title></title>
 	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="css/main.css">
 	<!--[if lt IE 9]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
 </head>
 <body>
 
-	<!-- CONTENT HERE -->
+	<header class="l-header">
+
+	</header>
+
+	<main class="l-main">
+		<!-- content -->
+	</main>
+
+	<footer class="l-footer">
+
+
+	</footer>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="js/jquery-1.10.1.min.js"><\/script>')</script>
-	<script src="scripts/1140-css3-mediaqueries.js" type="text/javascript"></script>
-	<script src="js/main.js"></script>
+	<script>window.jQuery || document.write('<script src="assets/js/jquery-1.10.1.min.js"><\/script>')</script>
+
+	<?php if(strstr($_SERVER['HTTP_HOST'], "8888")!=false) { ?>
+	    <script src="assets/js/src/global.js"></script>
+	<?php } else { ?>
+		<script src="assets/js/main.min.js"></script>
+	<?php } ?>
 
 </body>
 </html>
