aad043337344bd06d43d9a70db0e00f569a592d2
Tom Hopcraft <tomhop88@btinternet.com>
2015-04-07 12:26:41 +0100

    [js] - update html5shiv to use cdn

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="assets/css/main.css">
-	<!--[if lt IE 9]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+	<!--[if lt IE 9]> <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.js"></script> <![endif]-->
 </head>
 <body>
 
