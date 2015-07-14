675d736ac273a4c3aec9afb672013e1ef9b57632
Tom Hopcraft <tomhop88@btinternet.com>
2015-07-14 09:00:39 +0100

    [gulp] - hello - goodbye grunt

---
+++ Gulpfile.js
@@ -0,0 +1,81 @@
+// Processes
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    notify = require('gulp-notify'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync').create();
+
+// Paths
+var input = './assets/css/scss/**/*.scss',
+    output = './assets/css',
+    maps = './assets/css';
+
+// Options
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+var autoPrefixerOptions = {
+    browsers: ['last 10 versions', '> 5%', 'Firefox ESR']
+};
+
+// Tasks
+gulp.task('sass', function () {
+    return gulp
+        .src(input)
+        // Sourcemamps
+        //.pipe(sourcemaps.init())
+        .pipe(sass(sassOptions)).on('error', notify.onError(function (error) {
+            return "Problem file -> " + error.message;
+        }))
+
+        // Write the sourcemap
+        //.pipe(sourcemaps.write())
+        // Prefix that shit
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        // This adds all files except main
+        './assets/js/libs/*.js',
+        './assets/js/src/*.js'
+        ])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('compress', function() {
+    return gulp.src('./assets/js/main.js')
+        .pipe(uglify())
+        // @TODO - Need to output this to main.min.js not just main.js
+        .pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('watch', function() {
+    return gulp
+        .watch(input, ['sass']);
+});
+
+gulp.task('sass-compress', function () {
+    return gulp
+        .src(input)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer(autoPrefixerOptions))
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('sync', function() {
+    browserSync.init({
+        open: false,
+        proxy: "localhost:8888"
+    });
+});
+
+gulp.task('default', ['sync', 'watch']);
+gulp.task('deploy', ['sass-compress', 'scripts']);
